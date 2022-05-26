5dce8884d85d54bca092c91ddc8ab9bd08c29587
SulX_lj39 <SulX_lj39@student.itstep.org>
2022-05-26 22:33:34 +0200

    Adding saving menu

--- UI/Menu/Menu.h
+++ UI/Menu/Menu.h
@@ -8,45 +8,30 @@ struct Menu {
 		int choise;
 		cout << "Добро пожаловать в морской бой!!!" << endl;
 		do {
-			cout << "1. Выбрать режим игры" << endl << "2. Правила игры" << endl << "3. Выход" << endl;
+			cout << "1. Новая игра" << endl << "2. Продолжить игру" << endl << "3. Правила игры" << endl << "4. Выход" << endl;
 			cin >> choise;
 			system("cls");
 			switch (choise) {
-			case 1:
+			case 1: {
 				Gamemode(a.autoPut, b.autoPut, a.autoPlay, b.autoPlay);
-				break;
-			case 2:
+				int playerNumber = 1;
+				a.SettingFlot(playerNumber);
+				playerNumber = 2;
+				b.SettingFlot(playerNumber);
+			}break;
+			case 2: {
+				LoadGame(a, "Files\\Player 1.bin");
+				LoadGame(b, "Files\\Player 2.bin");
+			}break;
+			
+			case 3:
 				printRules();
 				break;
 			default:exit(0);
 			}
 			system("cls");
-		} while (choise != 1);
-		/*do
-		{
-			cout << "1. Новая игра" << endl;
-			cout << "2. Продолжить игру" << endl;
-			cin >> choise;
-			switch (choise)
-			{
-			case 1: {
-				int playerNumber = 1;
-				a.SettingFlot(playerNumber);
-				playerNumber = 2;
-				b.SettingFlot(playerNumber); }break;
-			case 2: {
-				LoadGame(a, "Files\\Player 1.bin");
-				LoadGame(b, "Files\\Player 2.bin");
-				}break;
-			default:
-				break;
-			}
-			break;
-		} while (true);*/
-		/*int playerNumber = 1;
-		a.SettingFlot(playerNumber);
-		playerNumber = 2;
-		b.SettingFlot(playerNumber);*/
+		} while (choise != 1&&choise!=2);
+	
 		cout << "Нажмите любую клавишу что бы начать игру" << endl;
 		_getch();
 	}
@@ -71,6 +56,7 @@ void printRules() {
 	cout << "1.\t= - пустая клетка " << endl << "\tS - корабль " << endl << "\tХ - Уничтоженый корабль" << endl;
 	cout << "2.\tКорабли нельзя располагать рядом друг с другом" << endl << "\t(только так)\n";
 	cout << "\t  0 1 2 3 4 5 6 7 8 9" << endl << "\tA = = = = = = = = = =" << endl << "\tB = = = = = S S S = =" << endl << "\tC = = S S S = = = = =" << endl << "\tD = = = = = = = = = =" << endl;
+	cout << "Для сохранения игры введите \"save\""<<endl;
 	system("pause");
 	system("cls");
 }
